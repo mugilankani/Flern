f08ed53fd3bafdd85d0647e9a3f3f446b6b444b9
abrahamjeron <abrahamjeron40@gmail.com>
2024-11-08 02:38:50 +0530

    done the leaderboard

---
+++ client/src/data/userData.js
@@ -0,0 +1,42 @@
+import userAvatar from "../assets/userAvatar.png";
+
+const userData = [
+    {  
+        userName: "Dhivya Barathi",
+        userAvatar: userAvatar,
+        points: 50,
+        league:"Bronze"
+    },
+    {
+        userName: "Dhivya Barathi iyal christopher",
+        userAvatar: userAvatar,
+        points: 30,
+        league:"Bronze"
+    },
+    {
+        userName: "Dhivya Barathi iyal",
+        userAvatar: userAvatar,
+        points: 30,
+        league:"Bronze"
+    },
+    {
+        userName: "Dhivya Barathi",
+        userAvatar: userAvatar,
+        points: 30,
+        league:"Bronze"
+    },
+    {
+        userName: "Dhivya Barathi",
+        userAvatar: userAvatar,
+        points: 30,
+        league:"Bronze"
+    },
+    {
+        userName: "Dhivya Barathi",
+        userAvatar: userAvatar,
+        points: 30,
+        league:"Bronze"
+    },
+];
+
+export default userData;
