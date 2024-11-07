a096fc6c32024aa64f0367b11fbdf154c5c88bd0
abrahamjeron <abrahamjeron40@gmail.com>
2024-11-08 03:27:45 +0530

    done

--- client/src/components/CreateWork/content.jsx
+++ client/src/components/CreateWork/content.jsx
@@ -12,16 +12,31 @@ function Content() {
           <img className="h-10 w-auto" src={logo} alt="Logo" />
           <h1 className="text-[2rem] ml-2">Flearn</h1>
         </div>
-        
-        <textarea
-          className="bg-[#d4d4d4] rounded-xl w-full mt-4 p-4"
-          name=""
-          id=""
-          cols="60"
-          rows="1"
-          placeholder="Enter course details..."
-        ></textarea>
-        <button className="mt-6 flex justify-between space-x-2 bg-black text-white py-2 px-6 rounded-xl">
+        <div>
+            <label className="text-[1.2rem]"> Course Topic: </label>
+            <textarea
+            className="bg-[#d4d4d4] rounded-xl w-full mt-4 p-4"
+            name=""
+            id=""
+            cols="60"
+            rows="1"
+            placeholder="Enter course details..."
+            ></textarea>
+        </div>
+
+        <div>
+            <label className="text-[1.2rem]"> Course Subject: </label>
+            <textarea
+            className="bg-[#d4d4d4] rounded-xl w-full mt-4 p-4"
+            name=""
+            id=""
+            cols="60"
+            rows="1"
+            placeholder="Enter course details..."
+            ></textarea>
+        </div>
+
+        <button className="mt-6 flex justify-between space-x-2 bg-black text-white py-2 px-6 rounded-3xl">
           Strat Creating
           <img className="pl-2" src={send} alt="" />
         </button>
